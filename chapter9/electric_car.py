48d36697505ac822ed953ef070b2097ea92a5caa
ye xaingyang <32386079+XiangyangYe@users.noreply.github.com>
2017-11-01 15:59:18 +0800

    Add files via upload

--- chapter9/electric_car.py
+++ chapter9/electric_car.py
@@ -1,4 +1,4 @@
-class Car(object):
+class Car():
 	"""docstring for Car"""
 	def __init__(self, make, model, year):
 		self.make = make
@@ -32,7 +32,7 @@ my_tesla = ElectricCar('tesla', 'model s', 2016)
 print(my_tesla.get_descriptive_name())
 ##############################################################################
 #给子类定义属性和方法
-class Car(object):
+class Car():
 	"""docstring for Car"""
 	def __init__(self, make, model, year):
 		self.make = make
@@ -71,7 +71,7 @@ print(my_tesla.get_descriptive_name())
 my_tesla.describe_battery() 
 
 #9.3.4重写父类的方法
-class Car(object):
+class Car():
 	"""docstring for Car"""
 	def __init__(self, make, model, year):
 		self.make = make
@@ -95,7 +95,9 @@ class Car(object):
 	def increment_odometer(self, miles):
 		self.odometer_reading += miles
 
-
+	def fill_gas_tank(self, gas):
+		self.gas = gas
+		print("This car need " + str(self.gas) + "-L gas.")
 
 
 class ElectricCar(Car):
@@ -107,12 +109,76 @@ class ElectricCar(Car):
 
 	def describe_battery(self):
 		print("This car has a " + str(self.battery_size) + "-kWh battery.")
+	"""重写父类，此方法对于子类不适用"""
+	def fill_gas_tank(self):
+		print("ElectricCar doesn't need gas!")
 
 
 
 my_tesla = ElectricCar('tesla', 'model s', 2016)
 print(my_tesla.get_descriptive_name())
 my_tesla.describe_battery() 
+my_tesla.fill_gas_tank()
+
+#9.3.5 将实例用作属性
+
+class Car():
+	"""docstring for Car"""
+	def __init__(self, make, model, year):
+		self.make = make
+		self.model = model
+		self.year = year
+		self.odometer_reading = 25
+
+	def get_descriptive_name(self):
+		long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+		return long_name.title()
+
+	def read_odometer(self):
+		print("This car has " + str(self.odometer_reading) + " miles on it.")
+
+	def update_odometer(self, mileage):
+		if mileage >= self.odometer_reading:
+			self.odometer_reading = mileage
+		else:
+			print("You Can't roll back an odometer")
+
+	def increment_odometer(self, miles):
+		self.odometer_reading += miles
+
+	def fill_gas_tank(self, gas):
+		self.gas = gas
+		print("This car need " + str(self.gas) + "-L gas.")
+
+class Battery():
+	def __init__(self, battery_size = 85):
+		self.battery_size = battery_size
+
+	def describe_battery(self):
+		print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+	def get_range(self):
+		if self.battery_size == 70:
+			range = 240
+		elif self.battery_size == 85:
+			range = 270
+			
+			message = "This car can go approximately " + str(range)
+			message += " miles on a full charge"
+			print(message)
+
+class ElectricCar(Car):
+	"""电动车的独特之处"""
+	def __init__(self, make, model, year):
+		"""初始化父类属性，再初始化电动汽车特有的属性"""
+		super().__init__(make, model, year)
+		self.battery = Battery()
+
+my_tesla = ElectricCar('tesla', 'model s', 2016)
+print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
+
 
 
  
