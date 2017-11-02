79e3605a5aa5b69cb157bf36d30ecce8497298d3
ye xaingyang <xiangyangye.fantacy@gmail.com>
2017-11-02 15:23:00 +0800

    Add files via upload

--- chapter10/abnormal/alice.py
+++ chapter10/abnormal/alice.py
@@ -1,9 +1,28 @@
 #--------------10.3.5 处理FileNotFoundError异常-----------------#
+# filename = 'alice.txt'
+
+# try:
+# 	with open(filename) as f_obj:
+# 		contents = f_obj.read()
+# except FileNotFoundError:
+# 	msg = "Sorry, the file " + filename + " does not exist."
+# 	print(msg)
+
+#------------------------10.3.6 分析文本-------------------------#
 filename = 'alice.txt'
 
+with open(filename, 'w') as file_object:
+	file_object.write("Alice in Wonderland.")
+
+#读文件，计算文件字符数
 try:
 	with open(filename) as f_obj:
 		contents = f_obj.read()
 except FileNotFoundError:
-	msg = "Sorry, the file " + filename + " does not exist."
+	msg = "Sorry, the title " + filename + "does not exist."
 	print(msg)
+else:
+	"""计算文件大致包含多少单词"""
+	words = contents.split()
+	num_words = len(words)
+	print("The file " + filename + " has about " + str(num_words) + " words.")
